257a592e419db7a9c8b8041bd42c59aec45d6865
Salvatore Chiricosta <chiricosta.salvatore27@gmail.com>
2024-05-07 09:35:42 +0200

    Aggiunta search bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -6,10 +6,23 @@ import {MatIconModule} from '@angular/material/icon';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ProfiloComponent } from '../profilo/profilo.component';
 import { HomeComponent } from '../home/home.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [MatSidenavModule, MatButtonModule, MatToolbarModule, MatIconModule, RouterLink, RouterOutlet, ProfiloComponent, HomeComponent],
+  imports: [MatSidenavModule, 
+            MatButtonModule, 
+            MatToolbarModule, 
+            MatIconModule, 
+            RouterLink, 
+            RouterOutlet, 
+            ProfiloComponent, 
+            HomeComponent, 
+            MatFormFieldModule, 
+            MatSelectModule, 
+            MatInputModule],    
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
